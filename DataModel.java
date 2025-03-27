fbfe5e9922565ed25c350a8d0339965c9c56e3a9
Renee Low <reneelow0205@gmail.com>
2025-03-27 18:49:09 +0000

    created a file for easier feature access

---
+++ DataModel.java
@@ -0,0 +1,13 @@
+/**************************************************************************
+ * Program description: model for storing each row of the csv
+ * Goals:
+ *      - should make it easier to organize the features fr
+ *      - stores features as objects so it's easy to access each feature
+ *      - fingers crossed hehe 
+ * Date: 27/03/25
+ * Author: Renee Low
+ ************************************************************************/
+
+public class DataModel {
+    
+}
